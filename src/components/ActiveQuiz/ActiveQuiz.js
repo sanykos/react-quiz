ace81c5ca3657785a0856ed9c59287002d9b674e
sanykos <sanykos446@gmail.com>
2020-07-01 01:47:11 +0300

    add quizlist

--- src/components/ActiveQuiz/ActiveQuiz.js
+++ src/components/ActiveQuiz/ActiveQuiz.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import classes from './ActiveQuiz.module.css'
+import AnswersList from './AnswersList/AnswersList'
 
 const ActiveQuiz = props => (
     <div className={classes.ActiveQuiz}>
@@ -10,12 +11,9 @@ const ActiveQuiz = props => (
             </span>
             <small>4 из 12</small>
         </p>
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-        </ul>
+        <AnswersList
+            answers={props.answers}
+        />
     </div>
 )
 
---
+++ src/components/ActiveQuiz/AnswersList/AnswersList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import classes from './AnswersList.module.css'
+import AnswerItem from './AnswerItem/AnswerItem'
+
+//
+const AnswersList = props => (
+   
+    <ul className={classes.AnswersList}>
+        {props.answers.map((answer, index) => {
+            return (
+                <AnswerItem key={index} 
+                    answer={answer}
+                />
+            )
+        })}
+    </ul>
+)
+
+export default AnswersList
