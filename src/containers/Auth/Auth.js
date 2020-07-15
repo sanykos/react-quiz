d28ac691c474d606e0d4b36d9b94dfb643fddb78
sanykos <sanykos446@gmail.com>
2020-07-15 23:18:27 +0300

    add auth css

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import classes from './Auth.module.css'
 
 export default class Auth extends React.Component {
     render() {
         return(
-            <div>
-                <h1>Auth</h1>
+            <div className={classes.Auth}>
+                <div>
+                    <h1>Auth</h1>
+                </div>
             </div>
         )
     }
